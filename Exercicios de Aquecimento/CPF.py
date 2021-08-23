16d4fa67bcfa82bc6b5d05e12d7175809a70ed3d
Vinicius Ruivo <88867743+VixxRuivo@users.noreply.github.com>
2021-08-23 19:23:37 -0300

    Update CPF.py

--- Exercicios de Aquecimento/CPF.py
+++ Exercicios de Aquecimento/CPF.py
@@ -1,8 +1,5 @@
 
 cpf = input("CPF(xxx.xxx.xxx-xx) :")
-
-
 while (cpf[3] !=".") or (cpf[7] !=".") or (cpf[11] !="-"):
         cpf = input("O formato deve ser: (xxx.xxx.xxx-xx) :")
-else:
-        print("O formato está correto")
+else:        print("O formato está correto")
