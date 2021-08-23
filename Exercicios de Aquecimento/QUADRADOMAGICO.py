d6a10e3ae5f4e4e24540063ad9f10a3efc2870c7
Vinicius Ruivo <88867743+VixxRuivo@users.noreply.github.com>
2021-08-23 19:25:01 -0300

    Update QUADRADOMAGICO.py

--- Exercicios de Aquecimento/QUADRADOMAGICO.py
+++ Exercicios de Aquecimento/QUADRADOMAGICO.py
@@ -1,11 +1,7 @@
 import random
-
-
 matriz = [[1, 2, 3], [4, 5, 6],[7, 8, 9]]
 resp = False
-
-def magic():
-    global resp
+def magic():    global resp
     if matriz[0][0] + matriz[1][0] + matriz[2][0] == matriz[0][1] + matriz[1][1] + matriz[2][1] == matriz[0][2] + \
             matriz[1][2] + matriz[2][2] == matriz[0][0] + matriz[0][1] + matriz[0][2] == matriz[1][0] + matriz[1][1] + \
             matriz[1][2] == matriz[2][0] + matriz[2][1] + matriz[2][2] == matriz[0][0] + matriz[1][1] + matriz[2][2] == \
@@ -14,7 +10,6 @@ def magic():
     else:
         res = False
     return resp
-
 while resp == False:
     seq = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     for i in range(3):
