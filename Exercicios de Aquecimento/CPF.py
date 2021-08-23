5c41d06360c40eef6c9d68e1e20ae029f295541c
Vinicius Ruivo <88867743+VixxRuivo@users.noreply.github.com>
2021-08-23 19:12:04 -0300

    Create CPF.py

---
+++ Exercicios de Aquecimento/CPF.py
@@ -0,0 +1,8 @@
+
+cpf = input("CPF(xxx.xxx.xxx-xx) :")
+
+
+while (cpf[3] !=".") or (cpf[7] !=".") or (cpf[11] !="-"):
+        cpf = input("O formato deve ser: (xxx.xxx.xxx-xx) :")
+else:
+        print("O formato está correto")
