a54e113cf715f45102f36445a557b4bcf1d3c0e9
Vinicius Ruivo <88867743+VixxRuivo@users.noreply.github.com>
2021-08-23 19:12:25 -0300

    Delete Imposto.py

--- Imposto.py
+++
@@ -1,9 +0,0 @@
-def somaImposto(Imposto, custo):
-    return custo + (custo * Imposto / 100)
-
-
-cost = float(input("Informe o valor do produto: R$ "))
-tax = float(input("Informe a taxa de juros: "))
-
-finalPrice = somaImposto(tax, cost)
-print(f"O valor final do produto é de R${finalPrice:.2f}")
